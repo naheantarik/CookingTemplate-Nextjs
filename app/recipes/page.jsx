2d61680249d28520d6b26a5aec3e5f0224412ec0
naheantarik <naheantarik@gmail.com>
2024-11-03 18:39:21 +0600

    Dynamic Home Created

--- app/recipes/page.jsx
+++ app/recipes/page.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import FoodCard from '../components/recipesCard/recipesCard';
+import GreenBG from '../../public/assets/image/green-bg.png'
+import FoodItem from '../../public/assets/image/food-plate.png'
 
 const RecipesPage = () => {
   return (
@@ -13,6 +16,19 @@ const RecipesPage = () => {
               <FoodCard />
               <FoodCard />
               <FoodCard />
+              <FoodCard />
+              <FoodCard />
+              <div className='w-[400px] h-[434px]'
+               style={{
+                backgroundImage: `url(${GreenBG.src})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+              >
+                <p className='font-mono text-[24px] font-normal leading-[30px] text-center text-white px-[114px] pt-[40px]'>Don't forget to eat healthy food</p>
+                <img src={FoodItem.src} alt="" />
+
+              </div>
               <FoodCard />
               <FoodCard />
               <FoodCard />
