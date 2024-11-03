a35ad663f1a7bb7c9b244337281a3065ef33eaae
naheantarik <naheantarik@gmail.com>
2024-11-03 15:11:46 +0600

    Recipes Section Design

--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import Image from "next/image";
 import HeaderTemplateComponent from "./components/HeaderTemplateComponents";
 import Hero from "./hero/page";
 import Categories from "./categories/page";
+import RecipesPage from "./recipes/page";
 
 export default function Home() {
 	return <>
 	<Hero />
-	<Categories />	
+	<Categories />
+	<RecipesPage />	
 	</>;
 }
---
+++ app/recipes/page.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import FoodCard from '../components/recipesCard/recipesCardComponent';
+
+const RecipesPage = () => {
+  return (
+    <section className='h-auto mx-auto max-w-[1280px] mt-[183px]'>
+        <div>
+            <div>
+                <h2 className='font-semibold font-Inter text-[48px] leading-[58px] text-center tracking-[-4%]'>Simple and tasty recipes</h2>
+                <p className='text-center text-[16px] leading-[28px] font-Inter '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ea adipisci hic nesciunt labore tenetur perspiciatis nisi voluptates reiciendis fugit!</p>
+            </div>
+            <div className='mt-[96px] grid grid-cols-3 gap-[40px]'>
+              <FoodCard />
+              <FoodCard />
+              <FoodCard />
+              <FoodCard />
+              <FoodCard />
+              <FoodCard />
+            </div>
+        </div>
+    </section>
+
+  );
+};
+
+export default RecipesPage;
