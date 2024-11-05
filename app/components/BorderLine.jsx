e5853795e900b65cabf7113f1b917590a91e6dc2
naheantarik <naheantarik@gmail.com>
2024-11-05 12:19:01 +0600

    Features Page Create

---
+++ app/components/BorderLine.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BorderLine = () => {
+  return (
+    <div className="items-center border-r border-black border-opacity-[10%] h-[50px]">
+      {" "}
+    </div>
+  );
+};
+
+export default BorderLine;
